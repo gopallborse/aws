3c0ba6a455d59d231ea3ff5491f644763ff82feb
Gopall Borse <gopallborse@gmail.com>
2025-04-22 16:22:40 +0530

    practice with notes

--- 01-notes-crud-api/handler.js
+++ 01-notes-crud-api/handler.js
@@ -1,5 +1,50 @@
 "use strict";
 
+// --------------------------------------------------------------
+
+// module.exports.createNote = async (event) => {
+//   return {
+//     statusCode: 201,
+//     body: JSON.stringify("A new note is created!"),
+//   };
+// };
+
+// module.exports.updateNote = async (event) => {
+//   const noteId = event.pathParameters.id;
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(`The note with id ${noteId} is updated!`),
+//   };
+// };
+
+// module.exports.deleteNote = async (event) => {
+//   const noteId = event.pathParameters.id;
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(`The note with id ${noteId} is deleted!`),
+//   };
+// };
+
+// module.exports.getAllNotes = async (event) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(`All notes`),
+//   };
+// };
+
+// module.exports.getNoteById = async (event) => {
+//   const noteId = event.pathParameters.id;
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(`A note with id ${noteId} is returned!`),
+//   };
+// };
+
+// --------------------------------------------------------------
+
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({ region: "ap-south-1" });
 
